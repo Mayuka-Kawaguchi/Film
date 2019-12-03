d8c5d9578d571021844e5ad3b33b4e6fa171c2d5
Mayuka-Kawaguchi <mayuka-150222@docomo.ne.jp>
2019-12-03 23:03:09 +0900

    first commit

--- Film/ViewController.swift
+++ Film/ViewController.swift
@@ -1,20 +1,51 @@
-//
-//  ViewController.swift
-//  Film
-//
-//  Created by K Mayuka on 2019/11/17.
-//  Copyright © 2019 K Mayuka. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var cameraButton: UIButton!
+    
+    
+    var captureSession = AVCaptureSession()
+    var mainCamera: AVCaptureDevice?
+    var innerCamera: AVCaptureDevice?
+    var currentDevice: AVCaptureDevice?
+    var photoOutput : AVCapturePhotoOutput?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func startCamera() {
+        setupCaptureSession()
+        captureSession.startRunning()
+        print("start")
+    }
+    
+    @IBAction func openAlbum() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            picker.allowsEditing = true
+            
+            present(picker, animated: true, completion: nil)
+//            self.performSegue(withIdentifier: "toFilter", sender: nil)
+        }
+    }
+    
+    func setupCaptureSession() {
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let storyboard: UIStoryboard = self.storyboard!
+        let next = storyboard.instantiateViewController(withIdentifier: "FilterViewController")
+        self.present(next, animated: true, completion: nil)
+        print("gggg")
+        
+//        originalImage = cameraImageView.image
+//        dismiss(animated: true, completion: nil)
+    }
 }
-
